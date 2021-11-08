c83173ab6910b0b0fe07fe70016d746442604be6
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2021-11-08 21:44:57 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs')
+const path = require('path')
 
+var folderFilesCopy = path.join('04-copy-directory/files-copy')
+var folderFiles = path.join('04-copy-directory/files')
+
+function copyDir() {
+   fs.mkdir(folderFilesCopy, () => { })
+   fs.readdir(folderFiles, (err, files) => {
+      files.forEach(file => {
+         fs.copyFile(folderFiles + '/' + file, folderFilesCopy + '/' + file, (err) => {
+            if (err) throw err
+         })
+      })
+   })
+   fs.readdir(folderFilesCopy, (err, files_copy) => {
+      files_copy.forEach(file_copy => {
+         fs.unlink(folderFilesCopy + '/' + file_copy, (err) => {
+            if (err) throw err
+         })
+      })
+   })
+}
+
+copyDir()
